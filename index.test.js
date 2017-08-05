364a82a5630e3844f2d26ba5a267e2e2d1d21a81
Alexander Patrick <ajgp1989@gmail.com>
2017-08-05 10:06:59 -0400

    Modified Test

--- index.test.js
+++ index.test.js
@@ -1,6 +1,7 @@
 test('Bot connects to Discord', done => {
     require('child_process').exec('node index.js --test 1000', (err, stdout, stderr) => {
-        expect(stdout).toBe('Ready!\n'); // \n because console.log ends with a new line
+        expect(stderr).not.toBe('An invalid token was provided.\n');
+        expect(stdout).toBe('Ready!\n');
         done();
     });
-})
+});
