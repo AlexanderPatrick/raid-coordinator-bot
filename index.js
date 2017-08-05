18e466a31d873ce6260d36887e56b8c9b88046b8
Alexander Patrick <ajgp1989@gmail.com>
2017-08-05 17:01:23 -0400

    Add Port environment variable Setup Webserver to bind to port for Heroku Excluded coverage folder from coverage report.

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 require('dotenv').config();
 var args = require('minimist')(process.argv.slice(2));
 
+
+// Setting up a webserver to bind to the port so that Heroku doesn't auto terminate
+var http = require('http');
+http.createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.end('<h1><center>RaidControllerBot is up!<center></h1>', 'utf-8');
+}).listen(process.env['PORT']);
+
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
