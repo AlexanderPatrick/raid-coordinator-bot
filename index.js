df81cf21d7a98aa8d6d9407bca0ce839d84d5277
Alexander Patrick <ajgp1989@gmail.com>
2017-08-12 23:38:39 -0400

    Added !caught, !ran and !tally commands. Refactored out some functions and wrote unit tests for them.

--- index.js
+++ index.js
@@ -12,8 +12,12 @@ http.createServer((request, response) => {
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const createListForChannelIfNotExists = require('./functions').createListForChannelIfNotExists;
+const addDefaultRaiderToRaiderListIfNotExist = require('./functions').addDefaultRaiderToRaiderListIfNotExist;
+const addDefaultRaiderTallyToTallyListIfNotExist = require('./functions').addDefaultRaiderTallyToTallyListIfNotExist;
 
-var raiderLists = {}
+var raiderLists = {};
+var tallyLists = {};
 client.on('message', (message) => {
     if (message.content === '?help') {
         var helpText = '';
@@ -21,6 +25,9 @@ client.on('message', (message) => {
         helpText += '!here - mark yourself as present at the raid.\n';
         helpText += '!notcoming - unmark yourself.\n';
         helpText += '!whocoming - list who is coming or here.\n';
+        helpText += '!caught - lets the bot know that the pokemon was caught.\n';
+        helpText += '!ran - lets the bot know that the pokemon ran.\n';
+        helpText += '!tally - Shows the tally for who caught it or it ran from.\n';
         message.channel.send(helpText);
     }
 
@@ -36,24 +43,15 @@ client.on('message', (message) => {
             return;
         }
 
-        if (!raiderLists.hasOwnProperty(message.channel.id)) {
-            raiderLists[message.channel.id] = [];
-        }
-
-        if (raiderLists[message.channel.id].map(raider => raider.name).includes(message.member.displayName)) {
-            raiderLists[message.channel.id][raiderLists[message.channel.id].map(raider => raider.name).indexOf(message.member.displayName)].count = count;
-            message.reply('Noted!');
-            return;
-        }
+        raiderLists = createListForChannelIfNotExists(raiderLists, message.channel.id);
+        raiderLists[message.channel.id] = addDefaultRaiderToRaiderListIfNotExist(raiderLists[message.channel.id], message.member.displayName);
         
-        raiderLists[message.channel.id].push({name: message.member.displayName, count: count, here: false});
+        raiderLists[message.channel.id][raiderLists[message.channel.id].map(raider => raider.name).indexOf(message.member.displayName)].count = count;
         message.reply('Noted.');
     }
 
     if (message.content === '!notcoming') {
-        if (!raiderLists.hasOwnProperty(message.channel.id)) {
-            raiderLists[message.channel.id] = [];
-        }
+        raiderLists = createListForChannelIfNotExists(raiderLists, message.channel.id);
 
         if (!raiderLists[message.channel.id].map(raider => raider.name).includes(message.member.displayName)) {
             message.reply('Okay.');
@@ -65,15 +63,8 @@ client.on('message', (message) => {
     }
 
     if (message.content === '!here') {
-        if (!raiderLists.hasOwnProperty(message.channel.id)) {
-            raiderLists[message.channel.id] = [];
-        }
-
-        if (!raiderLists[message.channel.id].map(raider => raider.name).includes(message.member.displayName)) {
-            raiderLists[message.channel.id].push({name: message.member.displayName, count: 1, here: true});
-            message.reply('Noted.');
-            return;
-        }
+        raiderLists = createListForChannelIfNotExists(raiderLists, message.channel.id);
+        raiderLists[message.channel.id] = addDefaultRaiderToRaiderListIfNotExist(raiderLists[message.channel.id], message.member.displayName);
         
         raiderLists[message.channel.id][raiderLists[message.channel.id].map(raider => raider.name).indexOf(message.member.displayName)].here = true;
         message.reply('Noted.');
@@ -89,6 +80,46 @@ client.on('message', (message) => {
         var raiderCountString = '*' + raiderCount + ' Player' + (raiderCount != 1 ? 's' : '') + '*\n';
         var comingList = raiderLists[message.channel.id].reduce((list, raider) => list + raider.name + (raider.count > 1 ? ' *x' + raider.count + '*': '') + (raider.here ? ' - *Here*\n' : '\n'), '');
         message.channel.send(raiderCountString + comingList);
+    }
+
+    if (/^!caught( x\d)?$/.test(message.content)) {
+        var match = message.content.match(/^!caught x(\d)$/);
+        var count = 1;
+        if (match != null && match.length == 2) {
+            count = parseInt(match[1]);
+        }
+
+        tallyLists = createListForChannelIfNotExists(tallyLists, message.channel.id);
+        tallyLists[message.channel.id] = addDefaultRaiderTallyToTallyListIfNotExist(tallyLists[message.channel.id],  message.member.displayName);
+
+        tallyLists[message.channel.id][tallyLists[message.channel.id].map(raider => raider.name).indexOf(message.member.displayName)].caught = count;
+        message.reply('Congrats' + (count > 0 ? '!'.repeat(count) : '?'));
+    }
+
+    if (/^!ran( x\d)?$/.test(message.content)) {
+        var match = message.content.match(/^!ran x(\d)$/);
+        var count = 1;
+        if (match != null && match.length == 2) {
+            count = parseInt(match[1]);
+        }
+
+        tallyLists = createListForChannelIfNotExists(tallyLists, message.channel.id);
+        tallyLists[message.channel.id] = addDefaultRaiderTallyToTallyListIfNotExist(tallyLists[message.channel.id],  message.member.displayName);
+        
+        tallyLists[message.channel.id][tallyLists[message.channel.id].map(raider => raider.name).indexOf(message.member.displayName)].ran = count;
+        message.reply('better luck next raid.');
+    }
+
+    if (message.content === '!tally') {
+        if (!tallyLists.hasOwnProperty(message.channel.id) || tallyLists[message.channel.id].length == 0) {
+            message.channel.send('**Tally:** Caught: 0, Ran:0');
+            return;
+        }
+
+        var caughtCount = tallyLists[message.channel.id].reduce((sum, raider) => sum + raider.caught, 0);
+        var ranCount = tallyLists[message.channel.id].reduce((sum, raider) => sum + raider.ran, 0);
+        var tallyString = '**Tally:** Caught: ' + caughtCount + ', Ran: ' + ranCount;
+        message.channel.send(tallyString);
     }
 });
 
